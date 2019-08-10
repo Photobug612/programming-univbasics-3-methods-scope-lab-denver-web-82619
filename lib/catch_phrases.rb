203b43494bacc91364d304179c1c5100577066aa
Arlene George <aemg@live.com>
2019-08-10 22:32:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,10 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  say = "It's Dangerout to Go Alone! Take This."
+  puts say
+end
+
+def any_phrase
